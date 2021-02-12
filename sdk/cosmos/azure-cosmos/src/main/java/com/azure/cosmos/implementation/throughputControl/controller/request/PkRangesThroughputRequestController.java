5b8f448dca94a4324a75106552246bf713cf12cc
annie-mac <annie-mac@Gurpreet.fareast.corp.microsoft.com>
2021-02-11 23:31:53 -0800

    no request controller

--- sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/throughputControl/controller/request/PkRangesThroughputRequestController.java
+++ sdk/cosmos/azure-cosmos/src/main/java/com/azure/cosmos/implementation/throughputControl/controller/request/PkRangesThroughputRequestController.java
@@ -123,22 +123,23 @@ public class PkRangesThroughputRequestController implements IThroughputRequestCo
 
     @Override
     public <T> Mono<T> processRequest(RxDocumentServiceRequest request, Mono<T> nextRequestMono) {
-        PartitionKeyRange resolvedPkRange = request.requestContext.resolvedPartitionKeyRange;
-
-        // If we reach here, it means we should find the mapping pkRange
-        ThroughputRequestThrottler requestThrottler =
-            this.getOrCreateRegionRequestThrottlers(this.globalEndpointManager.resolveServiceEndpoint(request))
-                .get(resolvedPkRange.getId());
-
-        if (requestThrottler != null) {
-            return requestThrottler.processRequest(request, nextRequestMono);
-        } else {
-            logger.warn(
-                "Can not find matching request throttler to process request {} with pkRangeId {}",
-                request.getActivityId(),
-                resolvedPkRange.getId());
-            return nextRequestMono;
-        }
+        return nextRequestMono;
+//        PartitionKeyRange resolvedPkRange = request.requestContext.resolvedPartitionKeyRange;
+//
+//        // If we reach here, it means we should find the mapping pkRange
+//        ThroughputRequestThrottler requestThrottler =
+//            this.getOrCreateRegionRequestThrottlers(this.globalEndpointManager.resolveServiceEndpoint(request))
+//                .get(resolvedPkRange.getId());
+//
+//        if (requestThrottler != null) {
+//            return requestThrottler.processRequest(request, nextRequestMono);
+//        } else {
+//            logger.warn(
+//                "Can not find matching request throttler to process request {} with pkRangeId {}",
+//                request.getActivityId(),
+//                resolvedPkRange.getId());
+//            return nextRequestMono;
+//        }
     }
 
     private double calculateThroughputPerPkRange() {
