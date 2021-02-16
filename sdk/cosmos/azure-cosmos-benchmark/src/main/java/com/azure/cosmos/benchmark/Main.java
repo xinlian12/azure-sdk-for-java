188bf62b6939c6488afe96b139dee76e5272052d
annie-mac <annie-mac@s-pkges.redmond.corp.microsoft.com>
2021-02-16 14:59:18 -0800

    some sample

--- sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/Main.java
+++ sdk/cosmos/azure-cosmos-benchmark/src/main/java/com/azure/cosmos/benchmark/Main.java
@@ -3,17 +3,19 @@
 
 package com.azure.cosmos.benchmark;
 
+import com.azure.cosmos.CosmosAsyncClient;
+import com.azure.cosmos.CosmosAsyncContainer;
+import com.azure.cosmos.CosmosAsyncDatabase;
+import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.benchmark.ctl.AsyncCtlWorkload;
 import com.azure.cosmos.benchmark.linkedin.LICtlWorkload;
-import com.beust.jcommander.JCommander;
+import com.azure.cosmos.models.PartitionKey;
 import com.beust.jcommander.ParameterException;
-import java.util.Optional;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.azure.cosmos.benchmark.Configuration.Operation.CtlWorkload;
-import static com.azure.cosmos.benchmark.Configuration.Operation.LinkedInCtlWorkload;
-import static com.azure.cosmos.benchmark.Configuration.Operation.ReadThroughputWithMultipleClients;
+import java.util.Optional;
 
 public class Main {
 
@@ -25,29 +27,39 @@ public class Main {
             Configuration cfg = new Configuration();
             cfg.tryGetValuesFromSystem();
 
-            JCommander jcommander = new JCommander(cfg, args);
-            if (cfg.isHelp()) {
-                // prints out the usage help
-                jcommander.usage();
-                return;
-            }
+            CosmosAsyncClient cosmosAsyncClient = new CosmosClientBuilder()
+                .endpoint(cfg.getServiceEndpoint())
+                .key(cfg.getMasterKey())
+                .buildAsyncClient();
 
-            validateConfiguration(cfg);
+            //force an exception
+            CosmosAsyncDatabase database = cosmosAsyncClient.getDatabase("non_existing_database");
+            CosmosAsyncContainer container = database.getContainer("non_existing_container");
+            container.readItem("itemId", new PartitionKey("partitionKey"), JsonNode.class).block();
 
-            if (cfg.isSync()) {
-                syncBenchmark(cfg);
-            } else {
-                if(cfg.getOperationType().equals(ReadThroughputWithMultipleClients)) {
-                    asyncMultiClientBenchmark(cfg);
-                } else if(cfg.getOperationType().equals(CtlWorkload)) {
-                    asyncCtlWorkload(cfg);
-                } else if (cfg.getOperationType().equals(LinkedInCtlWorkload)) {
-                    linkedInCtlWorkload(cfg);
-                }
-                else {
-                    asyncBenchmark(cfg);
-                }
-            }
+//            JCommander jcommander = new JCommander(cfg, args);
+//            if (cfg.isHelp()) {
+//                // prints out the usage help
+//                jcommander.usage();
+//                return;
+//            }
+//
+//            validateConfiguration(cfg);
+//
+//            if (cfg.isSync()) {
+//                syncBenchmark(cfg);
+//            } else {
+//                if(cfg.getOperationType().equals(ReadThroughputWithMultipleClients)) {
+//                    asyncMultiClientBenchmark(cfg);
+//                } else if(cfg.getOperationType().equals(CtlWorkload)) {
+//                    asyncCtlWorkload(cfg);
+//                } else if (cfg.getOperationType().equals(LinkedInCtlWorkload)) {
+//                    linkedInCtlWorkload(cfg);
+//                }
+//                else {
+//                    asyncBenchmark(cfg);
+//                }
+//            }
         } catch (ParameterException e) {
             // if any error in parsing the cmd-line options print out the usage help
             System.err.println("INVALID Usage: " + e.getMessage());
